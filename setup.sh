82f41cc750b70a29b31da124518711ac86f6530e
Jonas Arnklint <jonas.arnklint@nyxgg.com>
2015-06-29 18:01:37 +0200

    Setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/aliases ~/.aliases
-ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/aliases ~/.aliases
+ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
 
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+
+source ~/.bash_profile
 
 vim +PluginInstall +qall
